6ecfa669dc4f90ac263d075ae1a8a5f4e3258e7d
Trey Bean <trey@12spokes.com>
2010-03-06 18:51:27 -0700

    Applied templates from rspec_plugin generator to get specs to run.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'tandem'
-require 'spec'
-require 'spec/autorun'
-
-Spec::Runner.configure do |config|
-  
+begin
+  require File.dirname(__FILE__) + '/../../../../spec/spec_helper'
+rescue LoadError
+  puts "You need to install rspec in your base app"
+  exit
 end
+
+plugin_spec_dir = File.dirname(__FILE__)
+ActiveRecord::Base.logger = Logger.new(plugin_spec_dir + "/debug.log")
+
