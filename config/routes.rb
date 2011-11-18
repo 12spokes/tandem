3539eaf858bf5e38e7946e279801db5c95660f66
Jay Crouch <jay.crouch@me.com>
2011-11-18 13:04:51 -0700

    Tweaking configuration so that Tandem works in a 3.1 env

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :tandem_pages do |tandem_page|
-    tandem_page.resources :tandem_contents
-  end
-  
-  map.resources :tandem_texts
-end
+#ActionController::Routing::Routes.draw do |map|
+#  map.resources :tandem_pages do |tandem_page|
+#    tandem_page.resources :tandem_contents
+#  end
+#
+#  map.resources :tandem_texts
+#end
