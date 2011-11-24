4a187426dffd46eb6ac54958dd661c6e33e3602a
Jay Crouch <jay.crouch@me.com>
2011-11-23 22:09:36 -0700

    finally got cucumber completely working OMG!!!

--- spec/support/routes.rb
+++ spec/support/routes.rb
@@ -1,35 +1,6 @@
-module ControllerHacks
-  def get(action, parameters = nil, session = nil, flash = nil)
-    process_action(action, parameters, session, flash, "GET")
-  end
-
-  # Executes a request simulating POST HTTP method and set/volley the response
-  def post(action, parameters = nil, session = nil, flash = nil)
-    process_action(action, parameters, session, flash, "POST")
-  end
-
-  # Executes a request simulating PUT HTTP method and set/volley the response
-  def put(action, parameters = nil, session = nil, flash = nil)
-    process_action(action, parameters, session, flash, "PUT")
-  end
-
-  # Executes a request simulating DELETE HTTP method and set/volley the response
-  def delete(action, parameters = nil, session = nil, flash = nil)
-    process_action(action, parameters, session, flash, "DELETE")
-  end
-
-  private
-
-  def process_action(action, parameters = nil, session = nil, flash = nil, method = "GET")
-    parameters ||= {}
-    process(action, parameters.merge!(:use_route => :tandem), session, flash, method)
-  end
-end
-
 # This will include the routing helpers in the specs so that we can use
 # forum_path, forum_topic_path and so on to get to the routes.
 RSpec.configure do |config|
   config.include Tandem::Engine.routes.url_helpers
-  #config.include ControllerHacks, :type => :controller
   Tandem::Engine.load_engine_routes
 end
