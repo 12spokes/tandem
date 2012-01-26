e700b7f11c603df73add3df63f3951a7c6c719e8
Trey Bean <trey@12spokes.com>
2012-01-26 11:49:53 -0700

    [#24019525] Remove layout_path and unused javascript_path methods from application_controller in favor of just specifying the regular path.

--- app/controllers/tandem/application_controller.rb
+++ app/controllers/tandem/application_controller.rb
@@ -1,8 +1,6 @@
 module Tandem
   class ApplicationController < ActionController::Base
     include ApplicationControllerDevelopmentHack if defined? ApplicationControllerDevelopmentHack
-    layout :layout_path
-
     check_authorization
 
     rescue_from CanCan::AccessDenied do |exception|
@@ -15,16 +13,6 @@ module Tandem
 
     def current_ability
       @current_ability ||= Tandem::Ability.new(current_user)
-    end
-
-    def layout_path(layout)
-      return layout if layout.to_s.blank?
-      return layout.to_s[1..-1] if layout.to_s[0] == '/'
-      [Configuration.layouts_dir,layout.to_s].compact.join('/')
-    end
-
-    def javascript_layout
-      layout_path :javascript
     end
   end
 end
