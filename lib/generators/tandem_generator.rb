19c57be9eba000a7fa3e01553670cd9b7e104042
Trey Bean <trey@12spokes.com>
2012-01-27 10:40:51 -0700

    [Completes #24038309] Inject tandem assets into the host app in the tandem generator

--- lib/generators/tandem_generator.rb
+++ lib/generators/tandem_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators'
 
 class TandemGenerator < Rails::Generators::Base
-  desc "This generator creates an initializer file at config/initializers"
+  desc "This generator sets up tandem."
 
   def self.source_root
     @source_root ||= File.join(File.dirname(__FILE__), 'templates')
@@ -17,5 +17,10 @@ class TandemGenerator < Rails::Generators::Base
 
   def import_migrations
     rake("tandem:install:migrations")
+  end
+
+  def inject_tandem_assets
+    append_to_file 'app/assets/javascripts/application.js', '//= require tandem'
+    insert_into_file 'app/assets/stylesheets/application.css', " *= require tandem\n", :before => /^\*\/$/
   end
 end
