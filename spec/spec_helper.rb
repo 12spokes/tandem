10e5d48f0d2ab38ac2c834cafc2c3ac22316ab91
Trey Bean <trey@12spokes.com>
2012-02-03 13:49:36 -0700

    This is the line that was causing Warnings and Notices in the spec output. I think because we're using database_cleaner.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,7 +48,7 @@ Spork.prefork do
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
-    config.use_transactional_fixtures = true
+    # config.use_transactional_fixtures = true
 
     config.before(:suite) do
       DatabaseCleaner.strategy = :transaction
