5537c76b270b851e340572ce560cb86ecf35731e
Colleen Palmer <colleen@12spokes.com>
2012-02-22 20:04:02 -0700

    [#24418185] changes to image gallery layout

--- app/helpers/tandem/contents_helper.rb
+++ app/helpers/tandem/contents_helper.rb
@@ -8,7 +8,7 @@ module Tandem
     end
 
     def image_content_tag(image_content, options = {}, format = nil)
-      options = {style: "width:#{Tandem::Image::THUMB_WIDTH}px;border:5px solid #ccc;"}.merge(options) if format == :thumb
+      options = {style: ""}.merge(options) if format == :thumb
       image_tag image_content_url(image_content, format), options
     end
   end
