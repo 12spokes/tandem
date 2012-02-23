2579becfaeb71f72a2863b96f53eaeb35bc7b9eb
Trey Bean <trey@12spokes.com>
2012-02-22 17:02:55 -0700

    [#22387287] you can't pass nil to url if s3 is the storage method on paperclip, so make :original the default.

--- app/helpers/tandem/contents_helper.rb
+++ app/helpers/tandem/contents_helper.rb
@@ -1,13 +1,11 @@
 module Tandem
   module ContentsHelper
 
-    def image_content_url(image_content, format = nil)
-      image_content.image.present? ?
-          image_content.image.resource.url(format) :
-          'tandem/blank_image.jpg'
+    def image_content_url(image_content, format = :original)
+      image_content.image.present? ? image_content.image.resource.url(format) : 'tandem/blank_image.jpg'
     end
 
-    def image_content_tag(image_content, options = {}, format = nil)
+    def image_content_tag(image_content, options = {}, format = :original)
       options = {style: "width:#{Tandem::Image::THUMB_WIDTH}px;border:5px solid #ccc;"}.merge(options) if format == :thumb
       image_tag image_content_url(image_content, format), options
     end
