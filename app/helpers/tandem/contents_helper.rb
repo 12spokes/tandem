0b8a2365ec3ae3e8e0b13fb02243c3043ca3ed6e
Trey Bean <trey@12spokes.com>
2012-02-24 08:26:26 -0700

    Fix for improper rebase. Thought I saved before resolving. Oops.

--- app/helpers/tandem/contents_helper.rb
+++ app/helpers/tandem/contents_helper.rb
@@ -5,13 +5,8 @@ module Tandem
       image_content.image.present? ? image_content.image.resource.url(format) : 'tandem/blank_image.jpg'
     end
 
-<<<<<<< HEAD
     def image_content_tag(image_content, options = {}, format = :original)
-      options = {style: "width:#{Tandem::Image::THUMB_WIDTH}px;border:5px solid #ccc;"}.merge(options) if format == :thumb
-=======
-    def image_content_tag(image_content, options = {}, format = nil)
       options = {style: ""}.merge(options) if format == :thumb
->>>>>>> [#24418185] changes to image gallery layout
       image_tag image_content_url(image_content, format), options
     end
   end
