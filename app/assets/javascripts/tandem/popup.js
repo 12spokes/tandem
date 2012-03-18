5c661b2648ae8305cbd5446c7f999b45236ae2f2
Trey Bean <trey@12spokes.com>
2012-03-18 11:38:29 -0600

    [#26616591] Create fewer compiled javascripts by adding everything to tandem.js and make the popup layout use tandem.js

--- app/assets/javascripts/tandem/popup.js
+++ app/assets/javascripts/tandem/popup.js
@@ -1,13 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-//
-//= require jquery
-//= require jquery_ujs
-//= require tandem/wymeditor/jquery.wymeditor.min
-//= require tandem/images
-//= require tandem/vendor/jquery.ui.widget
-//= require tandem/vendor/jquery-fileupload/jquery.iframe-transport.js
-//= require tandem/vendor/jquery-fileupload/jquery.fileupload.js
 
 $().ready(function () {
   $('form.tandem_content_editor').on('ajax:success', function(event, content, status, jqXHR){
