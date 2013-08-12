1da1feda899f6a6807eb23ef8b3905a154d70c44
Trey Bean <trey@12spokes.com>
2013-08-11 19:11:19 -0600

    Move request specs to spec/features per capybara 2 and latest rspec conventions

---
+++ spec/features/contents_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "Editing" do
+  include RequestHelpers::GeneralHelpers
+
+  describe "basic text content" do
+    it "changes the content", js: true do
+      visit root_path
+      edit_tandem_text_content(:tandem_sidebar, 'I love tandem!')
+
+      within '#content_text_tandem_sidebar' do
+        page.should have_content('I love tandem!')
+      end
+    end
+  end
+
+  describe "wysiwyg content" do
+    it "changes the content", js: true do
+      visit root_path
+      edit_tandem_text_content(:tandem_text_block, 'I love tandem!', wysiwyg: true)
+
+      within '#content_text_tandem_text_block' do
+        page.should have_content('I love tandem!')
+      end
+    end
+  end
+end
+
+describe "Tandem Content on non-tandem pages" do
+  describe "get /widgets" do
+    it "renders the widgets index and creates a tandem content items" do
+      visit '/widgets'
+    end
+  end
+end
+
