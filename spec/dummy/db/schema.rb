ff9b8cb58e133644526c0c7dd4d1f2b6516e6bef
Trey Bean <trey@12spokes.com>
2013-08-16 23:15:27 -0600

    Scratch that. Late in the night/week. When you run app:db:migrate, it pulls in the engine's migrations. No need to double them up. Also included updated schema file.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -65,18 +65,18 @@ ActiveRecord::Schema.define(:version => 30000000000001) do
   add_index "tandem_pages", ["parent_id"], :name => "index_tandem_pages_on_parent_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
